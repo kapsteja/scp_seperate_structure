835d8cc5e405846a47778f2538ba452540577546
kapsteja <93643561+kapsteja@users.noreply.github.com>
2026-02-03 08:47:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,17 +9,8 @@ terraform {
   cloud {
     organization = "YOUR_ORG_NAME"  # Replace with your Terraform Cloud organization name
 
-    workspaces {
-      # This configuration uses tags to organize workspaces
-      # Create two workspaces in Terraform Cloud:
-      # 1. scp-deny (for Deny policies)
-      # 2. scp-others (for Other policies)
-      # 
-      # To switch workspaces:
-      # cd Deny && terraform workspace select scp-deny
-      # cd others && terraform workspace select scp-others
-      tags = ["scp-management"]
-    }
+    # Workspace will be specified using backend.conf or TF_WORKSPACE env var
+    # This allows different directories to use different workspaces
   }
 }
 
