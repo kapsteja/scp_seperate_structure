acb5b55a82a3c71953518fc9ee961e96ddc845a7
kapsteja <93643561+kapsteja@users.noreply.github.com>
2026-02-03 08:42:49 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,15 @@
-output "scp_ids" {
-  value = { for k, v in aws_organizations_policy.scp : k => v.id }
+output "deny_scp_ids" {
+  value = { for k, v in aws_organizations_policy.deny_scp : k => v.id }
 }
 
-output "scp_names" {
-  value = [for k, v in aws_organizations_policy.scp : v.name]
+output "deny_scp_names" {
+  value = [for k, v in aws_organizations_policy.deny_scp : v.name]
+}
+
+output "other_scp_ids" {
+  value = { for k, v in aws_organizations_policy.other_scp : k => v.id }
+}
+
+output "other_scp_names" {
+  value = [for k, v in aws_organizations_policy.other_scp : v.name]
 }
